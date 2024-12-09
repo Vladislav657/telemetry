20db5cdc1e76aff3082b336db474f15e82254452
vlad <ecscalibur_10@mail.ru>
2024-12-09 21:10:51 +0300

    telemetry

--- src/TmData.java
+++ src/TmData.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class TmData {
     String num;
-    String time;
+    long time;
     String size;
     int type;
     int length;
@@ -12,14 +12,18 @@ public class TmData {
 
     TmData(byte[] buff, FileInputStream in){
         try {
-            this.num = getNum(buff);
-            this.time = getTime(buff, in);
-            this.size = getSize(buff, in);
-            this.type = getType(buff, in);
+            this.num = this.getNum(buff);
+            this.time = this.getTime(buff, in);
+            this.size = this.getSize(buff, in);
+            this.type = this.getType(buff, in);
             if (this.type == 3)
-                this.length = getLength(buff, in);
-            else
+                this.length = this.getLength(buff, in);
+            else if (this.type == 1)
                 this.length = 8;
+            else {
+                this.getLength(buff, in);
+                this.length = 4;
+            }
             this.data = new byte[1024];
             this.getData(in);
         }
@@ -28,42 +32,11 @@ public class TmData {
         }
     }
 
-    String getNum(byte[] buff) {
-        return String.format("%02x%02x", buff[0], buff[1]);
-    }
-
-    String getTime(byte[] buff, FileInputStream in) throws IOException {
-        int size = in.read(buff, 0, 4);
-        return String.format("%02x%02x%02x%02x", buff[0], buff[1], buff[2], buff[3]);
-    }
-
-    String getSize(byte[] buff, FileInputStream in) throws IOException {
-        int size = in.read(buff, 0, 1);
-        return String.format("%02x", buff[0]);
-    }
-
-    int getType(byte[] buff, FileInputStream in) throws IOException {
-        int size = in.read(buff, 0, 1);
-        return buff[0];
-    }
-
-    int getLength(byte[] buff, FileInputStream in) throws IOException {
-        int size = in.read(buff, 0, 4);
-        int len = buff[3];
-        if (len < 0)
-            len = 128 * 2 + len;
-        return len;
-    }
-
-    void getData(FileInputStream in) throws IOException {
-        int size = in.read(this.data, 0, this.length);
-    }
-
-    void putToFile(FileWriter out){
+    public void putToFile(FileWriter out){
         try {
             out.write(this.num);
             out.write("  ");
-            out.write(this.time);
+            out.write(this.getFormattedTime(this.time));
             out.write("  ");
             out.write(this.size);
             out.write("  ");
@@ -80,8 +53,47 @@ public class TmData {
         }
     }
 
-    boolean useful(){
+    public boolean useful(){
         return !this.num.equals("ffff");
+    }
+
+    private String getNum(byte[] buff) {
+        return String.format("%02x%02x", buff[0], buff[1]);
+    }
+
+    private long getTime(byte[] buff, FileInputStream in) throws IOException {
+        int size = in.read(buff, 0, 4);
+        return buff[0] * 256 * 256 * 256 + buff[1] * 256 * 256 + buff[2] * 256 + buff[3];
+    }
+
+    private String getSize(byte[] buff, FileInputStream in) throws IOException {
+        int size = in.read(buff, 0, 1);
+        return String.format("%02x", buff[0]);
+    }
+
+    private int getType(byte[] buff, FileInputStream in) throws IOException {
+        int size = in.read(buff, 0, 1);
+        return buff[0];
+    }
+
+    private int getLength(byte[] buff, FileInputStream in) throws IOException {
+        int size = in.read(buff, 0, 4);
+        int len = buff[3];
+        if (len < 0)
+            len = 128 * 2 + len;
+        return len;
+    }
+
+    private void getData(FileInputStream in) throws IOException {
+        int size = in.read(this.data, 0, this.length);
+    }
+
+    private String getFormattedTime(long time){
+        long hours = time / 1000 / 60 / 60;
+        long minutes = time / 1000 / 60 - hours * 60;
+        long seconds = time / 1000 - hours * 60 * 60 - minutes * 60;
+        long milliseconds = time - hours * 60 * 60 * 1000 - minutes * 60 * 1000 - seconds * 1000;
+        return String.format("%02d:%02d:%02d,%03d", hours, minutes, seconds, milliseconds);
     }
 
     private void putTypeThree(FileWriter out) throws IOException {
